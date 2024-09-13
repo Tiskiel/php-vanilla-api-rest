12c3ed0c04c4984e915761b639bb3fd7a56a0907
Wanty Jean-François <wanty.jf@gmail.com>
2024-09-13 17:42:37 +0200

    feat implementing the validator
    in progress

---
+++ app/Repositories/ValidatorRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+final class ValidatorRepository extends Repository
+{
+    public function exist(string $table, string $column, string $value): bool
+    {
+        $sql = "SELECT COUNT(*) FROM $table WHERE $column = :$column";
+
+        $statement = $this->pdo->prepare($sql);
+
+        $statement->execute([":$column" => $value]);
+
+        return $statement->fetchColumn() > 0;
+    }
+}
---
+++ app/Services/ValidatorService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use App\Repositories\ValidatorRepository;
+use InvalidArgumentException;
+
+final class ValidatorService
+{
+    private ValidatorRepository $_repository;
+    private UserRepository $_userRepository;
+
+    /**
+     * @var string[]
+     */
+    private array $allowedTables = ['users', 'roles', 'permissions'];
+    /**
+     * @var array<string, string[]>
+     */
+    private array $allowedColumns = [
+        'users' => ['uuid', 'first_name', 'last_name', 'updated_at'],
+    ];
+
+
+    /**
+     * @param \PDO|null $pdo
+     */
+    public function __construct(
+        private ?\PDO $pdo = null
+    )
+    {
+        $this->_repository = $this->pdo ? new ValidatorRepository($this->pdo) : new ValidatorRepository();
+        $this->_userRepository = $this->pdo ? new UserRepository($this->pdo) : new UserRepository();
+    }
+
+    public function exist(string $table, string $column, string $value): bool
+    {
+        if (!in_array($table, $this->allowedTables)) {
+            throw new InvalidArgumentException('Table not allowed');
+        }
+
+        if (!in_array($column, $this->allowedColumns[$table])) {
+            throw new InvalidArgumentException('Column not allowed');
+        }
+
+        return $this->_repository->exist($table, $column, $value);
+    }
+}
