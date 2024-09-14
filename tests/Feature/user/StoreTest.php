a672ac5ad8e08e42fe31e26f1546d33262082a67
Wanty Jean-François <wanty.jf@gmail.com>
2024-09-14 12:14:18 +0200

    feat add a user's tests

--- tests/Feature/user/StoreTest.php
+++ tests/Feature/user/StoreTest.php
@@ -2,6 +2,7 @@
 
 use App\Dto\UserCreateDto;
 use App\Repositories\UserRepository;
+use App\Services\UserService;
 
 it('should store a user', function () {
     $userCreateDto = new UserCreateDto('John', 'Doe');
@@ -9,4 +10,36 @@ it('should store a user', function () {
     $userRepository = new UserRepository($this->pdo);
 
     expect($userRepository->store($userCreateDto))->toBeTrue();
+});
+
+it('should not store a user without a first name using the service', function () {
+    $service = new UserService($this->pdo);
+    $errors = $service->store('', 'Doe');
+
+    expect($errors)->toBeArray();
+    expect($errors['first_name'])->toBe('First name is required');
+});
+
+it('should not store a user without a last name and first name using the service', function () {
+    $service = new UserService($this->pdo);
+    $errors = $service->store('', '');
+
+    expect($errors)->toBeArray();
+    expect(count($errors))->toBe(2);
+    expect($errors['first_name'])->toBe('First name is required');
+    expect($errors['last_name'])->toBe('Last name is required');
+});
+
+it('should not store a user without a last name using the service', function () {
+    $service = new UserService($this->pdo);
+    $errors = $service->store('John', '');
+
+    expect($errors)->toBeArray();
+    expect($errors['last_name'])->toBe('Last name is required');
+});
+
+it('should store a user using the service', function () {
+    $service = new UserService($this->pdo);
+
+    expect($service->store('Sacha', 'Ketchum'))->toBeTrue();
 });
