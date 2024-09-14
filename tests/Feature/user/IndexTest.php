d9768a2402447e499b6fd01a13b4f129aaf8eb59
Wanty Jean-François <wanty.jf@gmail.com>
2024-09-14 20:43:57 +0200

    Update IndexTest.php

--- tests/Feature/user/IndexTest.php
+++ tests/Feature/user/IndexTest.php
@@ -5,13 +5,15 @@ use App\Repositories\UserRepository;
 
 beforeEach(function () {
     $users = [];
-    $this->userDto = new UserCreateDto('John', 'Doe');
-    $this->userDtoTwo = new UserCreateDto('Jane', 'Doe');
-    $this->userDtoThree = new UserCreateDto('John', 'Smith');
+    $this->userDto = new UserCreateDto('Sasha', 'Ketchum');
+    $this->userDtoTwo = new UserCreateDto('Limule', 'Tempesta');
+    $this->userDtoThree = new UserCreateDto('Morticia', 'Addams');
+    $this->userDtoFour = new UserCreateDto('Gomez', 'Addams');
 
     $users[] = $this->userDto;
     $users[] = $this->userDtoTwo;
     $users[] = $this->userDtoThree;
+    $users[] = $this->userDtoFour;
 
     $this->repository = new UserRepository($this->pdo);
 
@@ -23,5 +25,51 @@ beforeEach(function () {
 it('should find all users', function () {
     $users = $this->repository->index();
 
-    expect($users)->toHaveCount(3);
+    expect($users)->toHaveCount(4);
+});
+
+it('should find all users by first name', function () {
+    $users = $this->repository->index('Sasha');
+
+    expect($users)->toHaveCount(1);
+    expect($users[0]['first_name'])->toBe('Sasha');
+});
+
+it('should find all users by last name', function () {
+    $users = $this->repository->index(null, 'Addams');
+
+    expect($users)->toHaveCount(2);
+    expect($users[0]['last_name'])->toBe('Addams');
+    expect($users[1]['last_name'])->toBe('Addams');
+});
+
+it('should find all users by first and last name', function () {
+    $users = $this->repository->index('Morticia', 'Addams');
+
+    expect($users)->toHaveCount(1);
+    expect($users[0]['first_name'])->toBe('Morticia');
+    expect($users[0]['last_name'])->toBe('Addams');
+});
+
+it('should find a user with a part of first name', function () {
+    $users = $this->repository->index('Ash');
+
+    expect($users)->toHaveCount(1);
+    expect($users[0]['first_name'])->toBe('Sasha');
+});
+
+it('should find a user with a part of last name', function () {
+    $users = $this->repository->index(null, 'Adda');
+
+    expect($users)->toHaveCount(2);
+    expect($users[0]['last_name'])->toBe('Addams');
+    expect($users[1]['last_name'])->toBe('Addams');
+});
+
+it('should find a user with a part of first and last name', function () {
+    $users = $this->repository->index('Mort', 'Adda');
+
+    expect($users)->toHaveCount(1);
+    expect($users[0]['first_name'])->toBe('Morticia');
+    expect($users[0]['last_name'])->toBe('Addams');
 });
