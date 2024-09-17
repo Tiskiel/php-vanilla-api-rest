1a3fbb14b83a6d39873571b6b39f6c3941a26984
Wanty Jean-François <wanty.jf@gmail.com>
2024-09-17 08:54:08 +0200

    feat open the cors
    - add a condition to permit the pre call from a front app with options

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -7,7 +7,16 @@ use App\Middlewares\JwtMiddleware;
 use Dotenv\Dotenv;
 use Routes\Router;
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $dotenv = Dotenv::createImmutable(__DIR__. '/../../');
 $dotenv->load();
 
